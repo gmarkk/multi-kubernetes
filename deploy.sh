2f22f51ce1fe06fc31d008f545ae82d5c5da568a
root <root@ip-172-31-21-161.ec2.internal>
2023-06-02 11:10:11 +0000

    Added Deploy Scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t markgaisinsky/multi-client:latest -t markgaisinsky/multi-client:$SHA -f ..client/Dockerfile ./client
+docker build -t markgaisinsky/multi-server:latest -t markgaisinsky/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t markgaisinsky/multi-worker:latest -t markgaisinsky/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push markgaisinsky/multi-client:latest
+docker push markgaisinsky/multi-worker:latest
+docker push markgaisinsky/multi-server:latest
+
+docker push markgaisinsky/multi-client:$SHA
+docker push markgaisinsky/multi-worker:$SHA
+docker push markgaisinsky/multi-server:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=markgaisinsky/multi-server
+kubectl set image deployments/worker-deployment worker=markgaisinsky/muilti-worker
+kubectl set image deployments/client-deployment client=markgaisinsky/multi-client
